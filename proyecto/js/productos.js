37266614396867809934274e5eed48958b855013
grupo_1_23036 <131479244+g23036@users.noreply.github.com>
2023-07-02 12:30:31 -0300

    Add files via upload

---
+++ proyecto/js/productos.js
@@ -0,0 +1,77 @@
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        productos:[],
+        url:'http://localhost:5000/productos', 
+        error:false,
+        cargando:true,
+        /*atributos para el guardar los valores del formulario */
+        id:0,
+        nombre:"", 
+        imagen:"",
+        stock:0,
+        precio:0,
+        stock_minimo:0,
+    }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.productos = data;
+                    this.cargando=false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        eliminar(producto) {
+            const url = this.url+'/' + producto;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        },
+        confirmarEliminar(id) {
+            if (confirm("¿Estás seguro de eliminar este artículo?")) {
+              // Si el usuario confirma, llama al método eliminar
+              this.eliminar(id);
+            }
+          },
+          
+        grabar(){
+            let producto = {
+                nombre:this.nombre,
+                precio: this.precio,
+                stock: this.stock,
+                imagen:this.imagen,
+                stock_minimo:this.stock_minimo,
+            }
+            var options = {
+                body:JSON.stringify(producto),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./productos.html";  
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabarr")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
